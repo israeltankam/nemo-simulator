871c7b08cd80a6492c8394d6a317ff3722e34438
Israël Tankam <70442689+israeltankam@users.noreply.github.com>
2025-06-10 14:20:52 +0200

    Update nemo.py

--- nemo.py
+++ nemo.py
@@ -402,7 +402,6 @@ elif main_tab == "Simulation":
     transposed_df = df.transpose()
     transposed_df = transposed_df.rename_axis('Year')
     st.data_editor(transposed_df.iloc[1:, 1:],num_rows="dynamic",use_container_width=True)
-        
     X = np.zeros(st.session_state.num_years+1)
     Y = np.zeros(st.session_state.num_years+1)
     Z = np.zeros(st.session_state.num_years+1)
